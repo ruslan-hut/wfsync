a16a92a4c71d4dd5a53ce12d8ff46ccb50921ffe
ruslanhut <dev@programmer.com.ua>
2025-06-30 22:25:27 +0200

    Add `bson` tags to `CheckoutParams` and `ClientDetails` structures for MongoDB support

--- entity/checkout-params.go
+++ entity/checkout-params.go
@@ -7,16 +7,16 @@ import (
 )
 
 type CheckoutParams struct {
-	ClientDetails *ClientDetails `json:"client_details" validate:"required"`
-	LineItems     []*LineItem    `json:"line_items" validate:"required,min=1,dive"`
-	Total         int64          `json:"total" validate:"required,min=1"`
-	Currency      string         `json:"currency" validate:"required,oneof=PLN EUR"`
-	OrderId       string         `json:"order_id" validate:"required,min=1,max=32"`
-	SuccessUrl    string         `json:"success_url" validate:"required,url"`
-	Created       time.Time      `json:"created"`
-	Closed        time.Time      `json:"closed,omitempty"`
-	Status        string         `json:"status"`
-	SessionId     string         `json:"session_id,omitempty"`
+	ClientDetails *ClientDetails `json:"client_details" bson:"client_details" validate:"required"`
+	LineItems     []*LineItem    `json:"line_items" bson:"line_items" validate:"required,min=1,dive"`
+	Total         int64          `json:"total" bson:"total" validate:"required,min=1"`
+	Currency      string         `json:"currency" bson:"currency" validate:"required,oneof=PLN EUR"`
+	OrderId       string         `json:"order_id" bson:"order_id" validate:"required,min=1,max=32"`
+	SuccessUrl    string         `json:"success_url" bson:"success_url" validate:"required,url"`
+	Created       time.Time      `json:"created" bson:"created"`
+	Closed        time.Time      `json:"closed,omitempty" bson:"closed"`
+	Status        string         `json:"status" bson:"status"`
+	SessionId     string         `json:"session_id,omitempty" bson:"session_id"`
 }
 
 func (c *CheckoutParams) Bind(_ *http.Request) error {
@@ -31,11 +31,11 @@ type LineItem struct {
 }
 
 type ClientDetails struct {
-	Name    string `json:"name" validate:"required"`
-	Email   string `json:"email" validate:"required,email"`
-	Phone   string `json:"phone"`
-	Country string `json:"country"`
-	ZipCode string `json:"zip_code"`
-	City    string `json:"city"`
-	Street  string `json:"street"`
+	Name    string `json:"name" bson:"name" validate:"required"`
+	Email   string `json:"email" bson:"email" validate:"required,email"`
+	Phone   string `json:"phone" bson:"phone"`
+	Country string `json:"country" bson:"country"`
+	ZipCode string `json:"zip_code" bson:"zip_code"`
+	City    string `json:"city" bson:"city"`
+	Street  string `json:"street" bson:"street"`
 }
