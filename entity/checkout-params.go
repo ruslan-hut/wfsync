354a5610de9e3dd1073cf43cd73bbe0f7cc87915
programador <rkhut@h2plt.energy>
2025-06-30 15:59:06 +0200

    Add client details and currency validation to checkout parameters

--- entity/checkout-params.go
+++ entity/checkout-params.go
@@ -6,8 +6,10 @@ import (
 )
 
 type CheckoutParams struct {
-	LineItems []LineItem `json:"line_items" validate:"required,min=1,dive"`
-	Total     int64      `json:"total" validate:"required,min=1"`
+	ClientDetails *ClientDetails `json:"client_details" validate:"required"`
+	LineItems     []*LineItem    `json:"line_items" validate:"required,min=1,dive"`
+	Total         int64          `json:"total" validate:"required,min=1"`
+	Currency      string         `json:"currency" validate:"required,oneof=PLN EUR"`
 }
 
 func (c *CheckoutParams) Bind(_ *http.Request) error {
@@ -18,4 +20,14 @@ type LineItem struct {
 	Name  string `json:"name" validate:"required"`
 	Qty   int64  `json:"qty" validate:"required,min=1"`
 	Price int64  `json:"price" validate:"required,min=1"`
+}
+
+type ClientDetails struct {
+	Name    string `json:"name" validate:"required"`
+	Email   string `json:"email" validate:"required,email"`
+	Phone   string `json:"phone"`
+	Country string `json:"country"`
+	ZipCode string `json:"zip_code"`
+	City    string `json:"city"`
+	Street  string `json:"street"`
 }
