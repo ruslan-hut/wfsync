c095cc1c18d65926214960cfe051bfd183194459
el hombre <ruslan.hoot@icloud.com>
2025-07-04 16:57:15 +0200

    Fix `error` attribute formatting in Telegram logger
    - Remove redundant `Key` inclusion for `error` attributes to simplify log message.

--- lib/logger/tghandler.go
+++ lib/logger/tghandler.go
@@ -60,7 +60,7 @@ func (h *TelegramHandler) Handle(ctx context.Context, record slog.Record) error 
 		// Add attributes from .With() calls
 		for _, attr := range h.attrs {
 			if attr.Key == "error" {
-				msg += fmt.Sprintf("\n%s: ```error %v ```", attr.Key, attr.Value)
+				msg += fmt.Sprintf("```error %v ```", attr.Value)
 			} else {
 				msg += bot.Sanitize(fmt.Sprintf("\n%s: %v", attr.Key, attr.Value))
 			}
